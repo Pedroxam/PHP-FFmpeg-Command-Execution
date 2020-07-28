23db0f1bf518cea91352c19d0183c28d5dec06e7
Amoo Pedram <Pedroxam@gmail.com>
2020-07-28 23:01:43 +0300

    Update start.php

--- start.php
+++ start.php
@@ -5,21 +5,29 @@
 * Email: pedroxam@gmail.com
 */
 
-$root = dirname(__FILE__);
+if(empty($_POST['code']) or empty($_POST['video']))
+	exit('error');
+
+$root   = dirname(__FILE__);
 
 $input  = $root . '/input/' . trim($_POST['video']);
 
 $output = $root . '/output/' . basename($_POST['video']);
 
-$command = str_ireplace(['INPUT','OUTPUT'], [$input,$output], trim($_POST['code']));
+$command = str_ireplace(['INPUT', 'OUTPUT'], [$input, $output], trim($_POST['code']));
 
 $log = './log.txt';
 
-if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN')
-{
-	pclose(popen("start /B " . $command . " 1> $log 2>&1", "r")); // windows
-}
-else
-{
-	shell_exec($command . " 1> $log 2>&1 >/dev/null &"); //linux
+//validate ffmpeg command
+if (strpos($_POST['code'], 'ffmpeg') !== false){
+
+	if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN')
+	{
+		pclose(popen("start /B " . $command . " 1> $log 2>&1", "r")); // windows
+	}
+	else
+	{
+		shell_exec($command . " 1> $log 2>&1 >/dev/null &"); //linux
+	}
+
 }
