4de7ce34888a593f349ade232d42962036991401
Amoo Pedram <Pedroxam@gmail.com>
2020-08-07 02:04:32 +0300

    Update start.php

--- start.php
+++ start.php
@@ -1,8 +1,6 @@
 <?php
 /*
-* By Pedram
-* Telegram: @Pedroxam
-* Email: pedroxam@gmail.com
+* By Pedroxam
 */
 
 if(empty($_POST['code']) or empty($_POST['video']))
@@ -10,17 +8,26 @@ if(empty($_POST['code']) or empty($_POST['video']))
 
 $root   = dirname(__FILE__);
 
-$input  = $root . '/input/' . trim($_POST['video']);
+$parameter  = trim($_POST['code']);
 
-$output = $root . '/output/' . basename($_POST['video']);
+$video  = trim($_POST['video']);
 
-$command = str_ireplace(['INPUT', 'OUTPUT'], [$input, $output], trim($_POST['code']));
+$input  = $root . '/input/' . $video;
 
-$log = './log.txt';
+$output = $root . '/output/' . basename($video);
+
+// ffmpeg path
+// eg, for linux "/usr/bin/ffmpeg" and for windows, if you not set environment variables path, you can just enter path of ffmpeg.exe binary
+$ffmpeg = 'ffmpeg';
+
+$command = str_ireplace(['ffmpeg', 'INPUT', 'OUTPUT'], [$ffmpeg, $input, $output], $parameter);
+
+$log = $root . '/log.txt';
 
 //validate ffmpeg command
-if (strpos($_POST['code'], 'ffmpeg') !== false){
-
+if (strpos($parameter, 'ffmpeg') !== false){
+	
+	//Start Task
 	if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN')
 	{
 		pclose(popen("start /B " . $command . " 1> $log 2>&1", "r")); // windows
